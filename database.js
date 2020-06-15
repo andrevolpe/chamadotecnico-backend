efe9859f2564d14b413b05ecb899312a8a72685b
andrevolpe <39813143+andrevolpe@users.noreply.github.com>
2020-06-14 23:54:40 -0300

    Update database.js

--- database.js
+++ database.js
@@ -3,11 +3,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  user: 'gmttykaxjjcadz',
-   password: '38f7578d956485d37653bfa0560e47916de9e7b7ccee155e1fb971fe1a0d1d7b',
-   host: 'ec2-34-200-72-77.compute-1.amazonaws.com',
-   database: 'dbg23a9vo6hig0',
-   port: 5432,
+    user: 'igqzaployorpwy',
+    password: 'ed7ce222b0d00d486d78b8a4a65a631f7a3bba2d036ecee94e64b5f1a8bff92a',
+    host: 'ec2-34-194-198-176.compute-1.amazonaws.com',
+    database: 'd4not3pvst7vq8',
+    port: 5432,
     ssl: { rejectUnauthorized: false }
 
 });
@@ -16,8 +16,8 @@ const pool = new Pool({
 //    CREATE TABLE IF NOT EXISTS chamados
 //    (
 //        ID serial primary key,
- //       Titulo varchar(200),
-//        Data varchar(50),
+//        Chamado int,
+ //       Descricao varchar(300),
 //        Concluido boolean
 //    )
 //`;
@@ -29,10 +29,10 @@ const pool = new Pool({
 //})
 
 const sql_insert = `
-    INSERT INTO chamados(Titulo,Data,Concluido)
+    INSERT INTO chamados(Chamado,Descricao,Concluido)
     VALUES
-    ('Impressora com defeito', 05-05, true),
-    ('Impressora com defeito', 05-05, true)
+    (1234,'Impressora com defeito', true),
+    (1235, 'Impressora com defeito', true)
     `;
 
  pool.query(sql_insert, function(error, result){
